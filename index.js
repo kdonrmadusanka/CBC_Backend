744108bf8a052f791259c4d30066fdbb96356050
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-20 00:05:49 +0530

    Test the cnnection.

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ import mongoose from 'mongoose';
 
 const app = express();
 
-mongoose.connect(
+const connection = async () => {
+    await mongoose.connect(
     'mongodb+srv://admin:123456%40Tom@cbc-db.qoabxtl.mongodb.net/?retryWrites=true&w=majority&appName=cbc-db'
-).then(() => {console.log('Connection Successful')});
+    ).then(() => {console.log('Connection Successful')});
+
+    app.listen(3000, () => {console.log('App is opened in the post 3000')})
+}
+
+connection();
+
+app.get('/', (req, res) => {console.log('Test get request');
+    res.json({ 'message': 'test request'})});
