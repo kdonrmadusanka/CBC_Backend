0353043a26ea1e702a895c0ff6d61730b32aa062
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-19 23:36:38 +0530

    Connect MongDB database to the system.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-
+mongoose.connect(
+    'mongodb+srv://admin:123456%40Tom@cbc-db.qoabxtl.mongodb.net/?retryWrites=true&w=majority&appName=cbc-db'
+).then(() => {console.log('Connection Successful')});
