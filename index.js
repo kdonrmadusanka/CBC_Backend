8cb6c980e489b8205781a36db1ff393a6e3817cd
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-23 22:18:48 +0530

    Call the morgan library and use it as a middleware.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { config } from 'dotenv';
+import morgan from 'morgan';
 
 //Load environment variables
 config();
 
 const app = express();
+
+//Use morgan to load method
+app.use(morgan('dev'));
+
+//Enabling json body parsing
+app.use(express.json());
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 3000;
@@ -15,10 +22,11 @@ const connection = async () => {
     MONGODB_URI
     ).then(() => {console.log('Connection Successful')});
 
-    app.listen(PORT, () => {console.log('App is opened in the post 3000')})
+    app.listen(PORT, () => {console.log(`App is opened in the post ${PORT}`)})
 }
 
 connection();
 
 app.get('/', (req, res) => {console.log('Test get request');
-    res.json({ 'message': 'test request'})});
+    res.send(req.body);
+});
