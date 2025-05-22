739d2b9aef635e2508c36badf6375168ebaddc94
kdonrmadusanka <ravmkdon@gmail.com>
2025-05-22 10:21:22 +0530

    Updae the functionality of creating a product which auto increments the product id.

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -2,13 +2,30 @@ import Product from "../Models/product.js";
 
 export const createProduct = async (req, res) => {
     try {
-        const newProduct = new Product(req.body);
+        const latestProduct = await Product.find().sort({ _id: -1 }).limit(1);
+
+        let productId;
+        if (!latestProduct || latestProduct.length === 0) {
+            productId = 'P-0001';
+        } else {
+            const currentId = latestProduct[0].productId;
+            let number = currentId.replace('P-', '');
+            number = (parseInt(number, 10) + 1).toString().padStart(4, '0');
+            productId = 'P-' + number;
+        }
+
+        const newProduct = new Product({
+            ...req.body,
+            productId: productId
+        });
+
         await newProduct.save();
 
-        return res.status(201).json({ message: "New Product Created" });
+        return res.status(201).json({ message: "New Product Created", newProduct});
     } catch (error) {
         console.log(`The error message is ${error}.`);
-        
-        res.status(500).json({ message: "Failed to create product"});
+        return res.status(500).json({ message: "Failed to create product" });
     }
-}
+};
+
+
