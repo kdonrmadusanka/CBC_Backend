1c3825ad0950f7c9c9bb6d50abe833a56f5fe3a5
kdonrmadusanka <ravmkdon@gmail.com>
2025-05-22 11:03:36 +0530

    Add th functionality to get product by id.

--- Controllers/productController.js
+++ Controllers/productController.js
@@ -64,5 +64,20 @@ export const createProduct = async (req, res) => {
 };
 
 
+export const getProductById = async (req, res) => {
+    const { id } = req.params;
 
+    try {
+        const product = await Product.findOne({ productId: id });
+
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json(product);
+    } catch (error) {
+        console.error(`Error fetching product: ${error}`);
+        res.status(500).json({ message: "Server error" });
+    }
+};
 
--- Routes/productRoutes.js
+++ Routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from'express';
 import checkRole from '../Middleware/roleMiddleware.js';
-import { createProduct } from '../Controllers/productController.js';
+import { createProduct, getProductById } from '../Controllers/productController.js';
 
 const router = express.Router();
 
 router.post('/create', checkRole("admin"), createProduct);
+router.get('/:id', checkRole('admin'), getProductById);
 
 export default router;
